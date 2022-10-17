56e22e3e4829eb1c51e6b8828fe0a08deb93958d
nick335 <102188095+nick335@users.noreply.github.com>
2022-10-17 05:38:06 +0100

    country detail page animation has been completed!!

--- src/components/countryDetails.js
+++ src/components/countryDetails.js
@@ -37,7 +37,7 @@ export default function CountryDetails(props){
    const borderelement = countryData.map(each => {
     const potentialBorder = each.alpha3Code
     if( borders.includes(potentialBorder) ){
-      return <motion.h4 initial={{ y:50, opacity:0 }} animate={{ y:0, opacity:1 }} transition={{ duration:0.4, delay:0.5 }} className="trans bg-neutralText text-lightThemeText dark:text-neutralText dark:bg-darkThemeHeader shadow-1g px-6 py-1.5  rounded-md text-sm font-light w-fit mr-1.5 mb-2 " key={ nanoid() }>{ each.name }</motion.h4>
+      return <motion.h4 initial={{ y:30, opacity:0 }} whileInView ={{ y:0, opacity:1 }} transition={{ duration:0.4, delay:0.4 }} viewport = {{ once: true }} className="trans bg-neutralText text-lightThemeText dark:text-neutralText dark:bg-darkThemeHeader shadow-1g px-6 py-1.5  rounded-md text-sm font-light w-fit mr-1.5 mb-2 " key={ nanoid() }>{ each.name }</motion.h4>
     }
 
     return null;
@@ -57,19 +57,19 @@ export default function CountryDetails(props){
         <section className="mx-auto w-5/6 mt-8 lg:w-full lg:mx-0 lg:px-14 xl:px-20">
           <div className="w-fit">
             <Link to='/'>
-              <motion.h3 initial={{ y:50, opacity:0 }} animate={{ y:0, opacity:1 }} transition = {{ duration: 0.5}} className="trans bg-neutralText text-lightThemeText dark:text-neutralText dark:bg-darkThemeHeader shadow-md px-6 py-1 pt-1.5 rounded-md text-base font-semibold"><FontAwesomeIcon icon={ faArrowLeftLong } className='mr-1.5' /> Back</motion.h3>
+              <motion.h3 initial={{ y:50, opacity:0 }} whileInView ={{ y:0, opacity:1 }} transition = {{ duration: 0.5}} viewport = {{ once: true }} className="trans bg-neutralText text-lightThemeText dark:text-neutralText dark:bg-darkThemeHeader shadow-md px-6 py-1 pt-1.5 rounded-md text-base font-semibold"><FontAwesomeIcon icon={ faArrowLeftLong } className='mr-1.5' /> Back</motion.h3>
             </Link>
           </div>
           <div className="mt-12 lg:flex lg:items-center">
             <div className="lg:w-1/2">
-              <motion.img initial = {{ scale:0, opacity:0 }} animate = {{ scale:1, opacity:1 }} transition={{ duration:0.5 }} alt="country-big-flag" src={props.flag} className='lg:h-[340px] lg:w-5/6 object-cover h-52'/>
+              <motion.img initial = {{ scale:0, opacity:0 }} whileInView = {{ scale:1, opacity:1 }} transition={{ duration:0.5 }} alt="country-big-flag" src={props.flag} className='lg:h-[340px] lg:w-5/6 object-cover h-52'/>
             </div>
             <div className="mt-7 lg:w-1/2 lg:mt-0">
               <div className="lg:mb-3">
-                <motion.h3 initial={{ y:-50, opacity:0 }} animate={{ y:0, opacity:1 }} transition = {{ duration: 0.5}} className="trans text-lg font-extrabold text-lightThemeText dark:text-neutralText  lg:text-xl">{props.name}</motion.h3>
+                <motion.h3 initial={{ y:-50, opacity:0 }} whileInView={{ y:0, opacity:1 }} transition = {{ duration: 0.5}} className="trans text-lg font-extrabold text-lightThemeText dark:text-neutralText  lg:text-xl">{props.name}</motion.h3>
               </div>
-              <div className="lg:flex w-full lg:justify-between overflow-hidden"> 
-                <motion.div initial= {{ x: -130, opacity:0 }} animate = {{ x:0, opacity:1 }} transition = {{ duration: 0.5, delay:0.5 }} className="mt-4">
+              <div className="lg:flex w-full lg:justify-between overflow-hidden "> 
+                <motion.div initial= {{ x: -130, opacity:0 }} whileInView = {{ x:0, opacity:1 }} transition = {{ duration: 0.5, delay:0.6}} viewport = {{ once: true }}  className="mt-4">
                     <div className="flex mb-2.5 items-end">
                       <h3 className="trans mr-1.5 font-semibold text-base  text-lightThemeText dark:text-neutralText " >Native Name:</h3>
                       <h4 className='trans font-light text-base text-lightThemeText dark:text-neutralText '>{props.nativeName}</h4>
@@ -91,7 +91,7 @@ export default function CountryDetails(props){
                       <h4 className='trans font-light text-base text-lightThemeText dark:text-neutralText '>{props.capital}</h4>
                     </div>
                 </motion.div>
-                <motion.div className="mt-11 lg:mt-2.5" initial = {{ x:130, opacity:0 }} animate = {{ x:0, opacity:1 }} transition = {{ duration: 0.5, delay:0.5}}>
+                <motion.div className="mt-11 lg:mt-2.5" initial = {{ x:130, opacity:0 }} whileInView = {{ x:0, opacity:1 }} transition = {{ duration: 0.5, delay:0.6}} viewport = {{ once: true }} >
                   <div className="flex mb-2.5 items-end">
                     <h3 className="trans mr-1.5 font-semibold text-base  text-lightThemeText dark:text-neutralText " >Top Level Domain:</h3>
                     <h4 className='trans font-light text-base text-lightThemeText dark:text-neutralText '>{props.topleveldomain}</h4>
@@ -107,7 +107,7 @@ export default function CountryDetails(props){
                 </motion.div>
               </div>
               <div className="mt-6 lg:flex lg:flex-wrap lg:items-center overflow-hidden">
-                <motion.h3 initial={{ y:-50, opacity:0 }} animate={{ y:0, opacity:1 }} transition={{ duration: 0.5 }} className="trans font-semibold text-lightThemeText dark:text-neutralText lg:mr-3" >Border Countries:</motion.h3>
+                <motion.h3 initial={{ y:-15, opacity:0 }} whileInView={{ y:0, opacity:1 }} transition={{ duration: 0.5 }} viewport = {{ once: true }} className="trans font-semibold text-lightThemeText dark:text-neutralText lg:mr-3"  >Border Countries:</motion.h3>
                 <div className=" flex flex-wrap mt-2">
                   { borderelements }
                 </div>
